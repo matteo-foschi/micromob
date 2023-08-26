cf1f5b2272112e070c11df2f012927f121c5d1e2
matteo-foschi <matteo.foschi@ymail.com>
2023-08-26 21:54:49 +0200

    Last commit

--- app/auction/views.py
+++ app/auction/views.py
@@ -18,6 +18,7 @@ import json
 import redis
 
 from web3 import Web3
+from .utils import sendTransaction
 
 from dotenv import load_dotenv
 
@@ -208,37 +209,6 @@ def getUserHighBid(listId):
     else:
         userHighBid = "Be the first bidder"
     return userHighBid
-
-
-# Procedure to send the transaction on GOERLI TEST NET
-def sendTransaction(message):
-    w3 = Web3(
-        Web3.HTTPProvider(
-            "https://goerli.infura.io/v3/ecadd76dd30247b4bf4fd9238723bba2"
-        )
-    )
-    address = "0x3eDb1E13ae5D632a555128E57052B7662106DEa6"
-    # privateKey = os.getenv("privateKey") Not possibile to use .gitignore in PythonanyWhere
-    privateKey = "0xca4214d801b73e46f819ac5c492e813d50ab3e1b714d292878508e27c1f83b99"
-    nonce = w3.eth.get_transaction_count(address, "pending")
-    # w3.eth.get_transaction_count(address)
-    gasPrice = w3.eth.gas_price
-    value = w3.to_wei(0, "ether")
-    signedTx = w3.eth.account.sign_transaction(
-        dict(
-            nonce=nonce,
-            gasPrice=gasPrice,
-            gas=100000,
-            to="0x0000000000000000000000000000000000000000",
-            value=value,
-            data=message.encode("utf-8"),
-        ),
-        privateKey,
-    )
-
-    tx = w3.eth.send_raw_transaction(signedTx.rawTransaction)
-    txId = w3.to_hex(tx)
-    return txId
 
 
 # List of all the bid closed and filtered for username
